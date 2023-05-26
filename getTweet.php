1f4251ba4098217b89529de4b40ad4c244a2c09c
AntoineSP01 <113776815+AntoineSP01@users.noreply.github.com>
2023-05-26 17:52:10 +0200

    Delete getTweet.php

--- getTweet.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'config.php';
-
-$query = $conn->query("SELECT * FROM tweet ORDER BY tweet_date DESC LIMIT 1;");
-$tweet = $query->fetch(PDO::FETCH_ASSOC);
-
-echo json_encode($tweet);
-?>
